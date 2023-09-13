c6fc217bd8763e11e57d7bf176fa790f66082c56
askindogan <askindogan@hotmail.com>
2023-09-13 21:21:16 +0300

    commit

--- examples/bolum-6/fonksiyon-nedir.php
+++ examples/bolum-6/fonksiyon-nedir.php
@@ -1,7 +1,15 @@
 <?php
 
+//Fonksiyon Nedir?
 
+//Built-in Fonksiyonlar, dilin kendi içerisindeki hazır kütüthanelerle gelen fonksiyonlar
 
+echo date('Y/m/d');
+echo "<br>";
+echo strlen("Merhaba Dünya"); // örn karakter sayısını bildiren fonks.
+
+//3. üçcü taraf diğer kullanıcıların oluşturduğu kütüphaneler 
+//kullanıcı tanımlı bizim geliştirdiğimiz fonksiyonlar
 
 
 ?>
---
+++ examples/bolum-6/fonksiyon-tanimlama.php
@@ -0,0 +1,5 @@
+<?php
+
+//Fonksiyon tanımlama
+
+?>
