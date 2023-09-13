9f8a1e26785a9a04367f41cd9ae43b6db1003260
askindogan <askindogan@hotmail.com>
2023-09-13 22:54:31 +0300

    cc

--- examples/bolum-6/parametre-gonderme.php
+++ examples/bolum-6/parametre-gonderme.php
@@ -1,5 +1,32 @@
 <?php
+/*
+function selamlama($isim)
+{
+    return "Merhaba ".$isim."<br>";
+}
 
+echo selamlama("Aşkın");
+echo selamlama("Ömer");
+echo selamlama("Filiz");
+echo "<br>";
+*/
+function yasHesapla($dogumYili){
+    return date('Y')-$dogumYili;
+}
+echo "Merhaba Yaşınız: ".yasHesapla(1987);
+echo "<br>";
 
+function emeklilik($dogum,$isim)
+{
+    $yas=yasHesapla($dogum);
+    $kalanSure=65-$yas;
 
+    if ($kalanSure > 0) {
+        return "$isim, emekliliğe $kalanSure yıl kaldı.";
+    }else {
+        return "$isim zaten emeklisiniz.";
+    }
+}
+
+echo emeklilik(1987,"Aşkın");
 ?>
