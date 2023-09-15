9017c9dc1775ec6c13805546ddf2522ae28cb8d1
askindogan <askin-dogan@hotmail.com>
2023-09-15 16:56:37 +0300

    m

--- examples/bolum-6/uygulama-fonksiyonlar.php
+++ examples/bolum-6/uygulama-fonksiyonlar.php
@@ -1,29 +1,68 @@
 <?php
 //1-Kendisine gönderilen kelimeyi belirtilen kez ekranda gösteren fonksiyonu yazınız.
+echo "<b> 1-Kendisine gönderilen kelimeyi belirtilen kez ekranda gösteren fonksiyonu yazınız.</b>" . "<br>";
 
-function kelimeGoster($kelime, $adet){
-for ($i=0; $i <= $adet; $i+=1) { 
-echo "$kelime"."<br>";
+function kelimeGoster($kelime, $adet)
+{
+    for ($i = 0; $i <= $adet; $i += 1) {
+        echo "$kelime" . "<br>";
+    }
 }
-}
-kelimeGoster("1. sorunu cevabı" , 7);
+kelimeGoster("1. sorunu cevabı", 7);
 
 //2-Dikdörtgen alan ve çevresini hesaplayan fonksiyonu yazınız.
+echo "<b>2-Dikdörtgen alan ve çevresini hesaplayan fonksiyonu yazınız.</b>" . "<br>";
 
-function dikdortgenCevre($kisakenar,$uzunkenar)
+$kisakenar = 2;
+$uzunkenar = 3;
+
+echo "Kısa Kenar : " . $kisakenar . "<br>";
+echo "Uzun Kenar : " . $uzunkenar . "<br>";
+function dikdortgenCevre($kisakenar, $uzunkenar)
 {
-    return (($kisakenar+$uzunkenar)*2);
+    return (($kisakenar + $uzunkenar) * 2);
 }
-echo "Dikdörtgenin çevresi : ". dikdortgenCevre(5,3). "cm dir."."<br>";
+echo "Dikdörtgenin çevresi : " . dikdortgenCevre($kisakenar, $uzunkenar) . "cm dir." . "<br>";
 
-function dikdortgenAlan($a,$b)
+function dikdortgenAlan($kisakenar, $uzunkenar)
 {
-    return $a*$b;
+    return $kisakenar * $uzunkenar;
 }
 
-echo "Dikdörtgenin alanı : ". dikdortgenAlan(5,3). "cm dir."."<br>";
+echo "Dikdörtgenin alanı : " . dikdortgenAlan($kisakenar, $uzunkenar) . "cm dir." . "<br>";
+
 //3-Yazı tura uygulamasını fonksiyon kullanarak yapınız.
+echo "<b>3-Yazı tura uygulamasını fonksiyon kullanarak yapınız.</b>" . "<br>";
 
+function yaziTura()
+{
+    $sayi = rand(1, 100);
+    if ($sayi < 50) {
+        echo "Yazı";
+    } else {
+        echo "Tura";
+    }
+}
+
+yaziTura();
+echo "<br>";
 //4-Kendisine gönderilen bir sayının tam bölenlerini bir diziylşe döndüren fonksiyonu yazınız.
+echo "<b>4-Kendisine gönderilen bir sayının tam bölenlerini bir diziye döndüren fonksiyonu yazınız.</b>" . "<br>";
+
+function tamBolenleriBul($sayi)
+{
+    $tamBolenler = array();
+
+    for ($i = 2; $i < $sayi; $i++) {
+        if ($sayi % $i == 0) {
+            array_push($tamBolenler, $i);
+        }
+    }
+
+    return $tamBolenler;
+}
+
+print_r( tamBolenleriBul(20));
+
 
 ?>
