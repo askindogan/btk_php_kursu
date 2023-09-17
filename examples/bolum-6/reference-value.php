7f602ca627e616012d3035a4677b7585d9cbab3d
askindogan <askin-dogan@hotmail.com>
2023-09-17 23:45:52 +0300

    commti

--- examples/bolum-6/reference-value.php
+++ examples/bolum-6/reference-value.php
@@ -1,6 +1,17 @@
 <?php 
+//php de iki  türlü parametre gönderme şekli vardır
+//reference parametre ve value parametre
 
+function toplama($sayi){
+    $sayi+=10;
+    echo $sayi;
+}
 
+$ornekSayi=5;
 
+toplama($ornekSayi);
+echo "<br>";
+
+echo $ornekSayi;
 
 ?>
