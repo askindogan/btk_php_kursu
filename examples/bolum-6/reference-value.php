f5b2c40655a1732da822176cfb88ace7cfaa8cb8
askindogan <askin-dogan@hotmail.com>
2023-09-19 16:48:35 +0300

    commit

--- examples/bolum-6/reference-value.php
+++ examples/bolum-6/reference-value.php
@@ -4,14 +4,41 @@
 
 function toplama($sayi){
     $sayi+=10;
-    echo $sayi;
+    echo $sayi."<br>";
 }
 
 $ornekSayi=5;
+toplama($ornekSayi); //Burada ram üzerinde örneğin 001112 => 15 şeklinde değer dönderir
 
-toplama($ornekSayi);
+echo $ornekSayi; // burada ise bir kopyasını oluşturup 001113 => 5 değerini dönderir
+echo "<br>";
+echo "------------";
 echo "<br>";
 
-echo $ornekSayi;
+//referans gönderme & işareti ile yapılır ve iki değeride aynı şekilde değiştirir ve ram üzerinde tek bir değer tutulur
+function toplam(&$sayi){
+    $sayi+=10;
+    echo $sayi."<br>";
+}
 
+$ornekSayi=5;
+toplam($ornekSayi);
+
+echo $ornekSayi;
+echo "<br>";
+
+
+//Value değer gönderme işlemi
+function yasHesapla($tarihler){
+
+    for ($i=0; $i <count($tarihler) ; $i++) { 
+        $tarihler[$i]=2023-$tarihler[$i];
+    }
+    return $tarihler;
+}
+
+$liste=array(1987,1988,2016);
+echo "<pre>";
+print_r(yasHesapla($liste));
+echo "</pre>";
 ?>
