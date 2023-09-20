a641b9da9238b050500f38eac61a20094ec3279e
askindogan <askin-dogan@hotmail.com>
2023-09-20 11:52:54 +0300

    commmit

---
+++ examples/bolum-7/urunler.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="#">Anasayfa</a>
+            <a href="#">Ürünler</a>
+            <a href="#">İletişim</a>
+        </nav>
+    </header>
+    <main>
+        <h1>Ürün Listesi</h1>
+        <ul>
+            <li>Samsung S1</li>
+            <li>Samsung S2</li>
+            <li>Samsung S3</li>
+            <li>Samsung S4</li>
+            <li>Samsung S5</li>
+        </ul>
+        <nav>
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+        </nav>
+    </main>
+    <footer>
+        <p>
+            Tüm hakları saklıdır.
+        </p>
+    </footer>
+</body>
+</html>
