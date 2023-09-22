7cc8fd8e486842cb47b33fc68b6880e3f820dc4a
askindogan <askin-dogan@hotmail.com>
2023-09-22 14:45:47 +0300

    commit

---
+++ courseapp/uygulama-8/partials/_footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="container">
+            <p>Tüm hakları saklıdır.</p>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ courseapp/uygulama-8/partials/_navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+    <div class="container">
+        <a href="../" class="navbar-brand">CourseAPP</a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="/" class="nav-link active">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+            <a href="/" class="nav-link">Kurslar</a>
+        </li>
+    </ul>
+    </div> 
+</nav>
