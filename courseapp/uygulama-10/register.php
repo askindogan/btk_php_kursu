321d06734b6bae51ee9199546d35cc9436df84a0
askindogan <askin-dogan@hotmail.com>
2023-09-28 22:56:56 +0300

    commit

--- courseapp/uygulama-10/register.php
+++ courseapp/uygulama-10/register.php
@@ -7,19 +7,49 @@ require "libs/functions.php";
 
 <?php
 
+$usernameErr = $passwordErr = $emailErr = $repasswordErr = $cityErr = $hobilerErr = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $repassword = $_POST['repassword'];
 
-    echo $username . "<br>";
-    echo $email . "<br>";
-    echo $password . "<br>";
-    echo $repassword . "<br>";
+    $username = $email = $password = $repassword = $city = $hobiler = "";
+
+    if (empty($_POST['username'])) {
+        $usernameErr = "username alanı gerekli.";
+    } else {
+        $username = $_POST['username'];
+    }
+
+    if (empty($_POST['email'])) {
+        $emailErr = "email alanı gerekli.";
+    } else {
+        $email = $_POST['email'];
+    }
+
+    if (empty($_POST['password'])) {
+        $passwordErr = "password alanı gerekli.";
+    } else {
+        $password = $_POST['password'];
+    }
+
+    if (empty($_POST['repassword'])) {
+        $repasswordErr = "repassword alanı gerekli.";
+    } else {
+        $repassword = $_POST['repassword'];
+    }
+
+    if ($_POST['city'] == -1) {
+        $cityErr = "city alanı gerekli.";
+    } else {
+        $city = $_POST['city'];
+    }
+
+    if (!isset($_POST['hobiler'])) {
+        $hobilerErr = "hobiler alanı gerekli.";
+    } else {
+        $hobiler = $_POST['hobiler'];
+    }
+
 }
-
-
 
 ?>
 
@@ -31,18 +61,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="mb-3">
                     <label for="username">Kullanıcı Adı</label>
                     <input type="text" name="username" class="form-control">
+                    <div class="text-danger"><?php echo $usernameErr ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="email">Eposta</label>
                     <input type="email" name="email" class="form-control">
+                    <div class="text-danger"><?php echo $emailErr ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="password">Parola</label>
                     <input type="password" name="password" class="form-control">
+                    <div class="text-danger"><?php echo $passwordErr ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="repassword">Parola Tekrar</label>
                     <input type="password" name="repassword" class="form-control">
+                    <div class="text-danger"><?php echo $repasswordErr ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="city">Şehir</label>
@@ -54,17 +88,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         <option value="55">Samsun</option>
                         <option value="01">Adana</option>
                     </select>
+                    <div class="text-danger"><?php echo $cityErr ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="hobiler">Hobiler</label>
                     <div class="form-check">
                         <input type="checkbox" name="hobiler[]" id="hobiler_0" value="sinema">
-                        <label for="hobiler_0" class="form-check-label">sinema</label>
+                        <label for="hobiler_0" class="form-check-label">Sinema</label>
                     </div>
                     <div class="form-check">
                         <input type="checkbox" name="hobiler[]" id="hobiler_1" value="spor">
                         <label for="hobiler_1" class="form-check-label">Spor</label>
                     </div>
+                    <div class="text-danger"><?php echo $hobilerErr ?></div>
                 </div>
                 <button type="submit" class="btn btn-primary">Kayıt Ol</button>
             </form>
