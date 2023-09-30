e0c4bff73dba5e58b8790367644ba59e487f9e66
askindogan <askin-dogan@hotmail.com>
2023-09-30 21:00:51 +0300

    commit

--- examples/bolum-9/index.php
+++ examples/bolum-9/index.php
@@ -1,3 +1,40 @@
+<?php
+
+if (isset($_POST['btnFileUpload'])) {
+
+    $uploadOk = true;
+    $dest_path = "./uploadedFiles/";
+    $filename = $_FILES['fileToUpload']['name'];
+    $fileSize = $_FILES['fileToUpload']['size'];
+
+    if (empty($filename)) {
+        $uploadOk = false;
+        echo "Lütfen bir dosya seçiniz.";
+        echo "<br>";
+    }
+
+    if ($fileSize > 300000) {
+        $uploadOk = false;
+        echo "Dosya boyutu çok fazla.";
+        echo "<br>";
+    }
+
+    $fileSourcePath = $_FILES['fileToUpload']['tmp_name'];
+    $fileDestPath = $dest_path . $filename;
+
+    if (!$uploadOk) {
+        echo "Dosya yüklenemedi.";
+    } else {
+        if (move_uploaded_file($fileSourcePath, $fileDestPath)) {
+            echo "Dosya Yüklendi";
+        } else {
+            echo "Hay aksi olmadı :(";
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,9 +48,9 @@
 
     <!-- Bir form ile dosya gönderme işlemi için 
     form nesnesi içine mutlaka enctype="multipart/form-data" eklenmeli  -->
-    <form action="upload.php" method="post" enctype="multipart/form-data">
+    <form method="post" enctype="multipart/form-data">
         <input type="text" name="username">
-        <input type="file" name="fileToUpload" >
+        <input type="file" name="fileToUpload">
         <button type="submit" name="btnFileUpload">Yükle</button>
     </form>
 
