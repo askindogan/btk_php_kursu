51a722827fc8adba036645be72c9f4f8593c2748
askindogan <askin-dogan@hotmail.com>
2023-10-02 21:16:32 +0300

    commit

--- examples/bolum-10/cookies.php
+++ examples/bolum-10/cookies.php
@@ -3,14 +3,10 @@
 echo date("H:i:s")."<br>";
 echo time()."<br>";
 
+//Tanımlama
 setcookie("username","provzax",time()+(60*60*24));
 setcookie("auth","true",time()+(60*60*24)*24);
 
-echo "<pre>";
-print_r($_COOKIE);
-echo "</pre>";
-
-exit;
 if (isset($_COOKIE["username"])) {
     echo "Tanımlanan Cookie bilgisi: ";
     echo $_COOKIE["username"]."<br>";
@@ -19,3 +15,9 @@ if (isset($_COOKIE["username"])) {
 }
 echo "Tanımlanan Cookie bilgisi: ";
 echo $_COOKIE["auth"];
+
+//güncelleme -username cookiesi aşağıdaki şekilde güncellendi
+setcookie("username","askindogan",time()+(60*60*24));
+//Silme- şuandan önceki bir zamana ayarlarsak silinir
+setcookie("username","askindogan",time()-(60*60));
+
